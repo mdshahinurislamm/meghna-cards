fb75bdde03a510e4f3840a5701819f80e9e728c2
Md. Shahinur Islam <shahinalam6644@gmail.com>
2025-05-21 17:08:49 +0600

    3 categories added

--- meghna.sql
+++ meghna.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 06, 2025 at 12:53 PM
+-- Generation Time: May 21, 2025 at 01:08 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -69,7 +69,8 @@ INSERT INTO `categories` (`id`, `name`, `slug`, `status`, `created_at`, `updated
 (2, 'ONLINE FOOD DELIVERY1', 'online-food-delivery', '1', '2025-05-06 03:28:37', '2025-05-06 03:37:29'),
 (3, 'New', 'new', '1', '2025-05-06 03:55:57', '2025-05-06 03:55:57'),
 (4, 'Shahin', 'shahin', '1', '2025-05-06 04:12:50', '2025-05-06 04:12:50'),
-(5, 'category', 'category', '1', '2025-05-06 04:39:46', '2025-05-06 04:39:46');
+(5, 'category', 'category', '1', '2025-05-06 04:39:46', '2025-05-06 04:39:46'),
+(6, 'Current Offers', 'current-offers', '1', '2025-05-21 04:46:02', '2025-05-21 04:46:02');
 
 -- --------------------------------------------------------
 
@@ -294,15 +295,17 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `position`, `user_id`, `category_id`, `title`, `slug`, `content`, `content_css`, `excerpt`, `thumbnail_path`, `post_type`, `status`, `option_1`, `option_2`, `option_3`, `option_4`, `more_option_1`, `more_option_2`, `gallery_img`, `template`, `trash`, `created_at`, `updated_at`) VALUES
-(1, NULL, 1, '', 'Copyright', 'copyright', '<footer id=\"footer\" class=\"footer accent-background\">\r\n<div class=\"container copyright text-center \">\r\n<div class=\"social-links d-flex\" style=\"justify-content: center;\"><a><i class=\"bi bi-twitter-x\"></i></a> <a><i class=\"bi bi-facebook\"></i></a> <a><i class=\"bi bi-instagram\"></i></a> <a><i class=\"bi bi-linkedin\"></i></a></div>\r\n<p class=\"pt-3\">&copy; <span>Copyright</span> 2025 Meghna Bank PLC. All rights reserved.</p>\r\n</div>\r\n</footer><!-- Scroll Top -->\r\n<p><a href=\"#\" id=\"scroll-top\" class=\"scroll-top d-flex align-items-center justify-content-center\"><i class=\"bi bi-arrow-up-short\"></i></a></p>\r\n<!-- Preloader -->\r\n<div id=\"preloader\"></div>', NULL, NULL, NULL, 'footer', '1', NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, NULL, '2025-05-06 00:02:57', '2025-05-06 00:39:19'),
-(9, 1, 1, '', 'Home Page', 'home-page', NULL, NULL, NULL, NULL, 'posts', '1', NULL, NULL, NULL, NULL, NULL, NULL, '', 'meghna_slider,meghna_type', NULL, '2025-05-06 00:52:25', '2025-05-06 02:30:23'),
+(1, NULL, 1, '', 'Copyright', 'copyright', '<div class=\"social-links d-flex\" style=\"justify-content: center;\"><a><i class=\"bi bi-twitter-x\"></i></a> <a><i class=\"bi bi-facebook\"></i></a> <a><i class=\"bi bi-instagram\"></i></a> <a><i class=\"bi bi-linkedin\"></i></a></div>', NULL, NULL, NULL, 'footer', '1', NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, NULL, '2025-05-06 00:02:57', '2025-05-06 22:24:02'),
+(9, 1, 1, '', 'Home Page', 'home-page', NULL, NULL, NULL, NULL, 'posts', '1', NULL, NULL, NULL, NULL, NULL, NULL, '', 'meghna_slider,meghna_type', NULL, '2025-05-06 00:52:25', '2025-05-07 00:40:01'),
 (10, NULL, 1, '', 'Slider 1', 'test', '<p>testset</p>', NULL, NULL, '2025/05/6819b4c0bddf6_banner3.webp', 'meghna-slider', '1', NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, NULL, '2025-05-06 01:00:25', '2025-05-06 01:05:56'),
 (11, NULL, 1, '', 'Slider 2', 'slider-2', NULL, NULL, NULL, '2025/05/6819b4f1b5f5b_banner1.webp', 'meghna-slider', '1', NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, NULL, '2025-05-06 01:06:32', '2025-05-06 01:06:32'),
-(12, 1, 1, '2', '20% SAVINGS', 'test-1', '<div class=\"modal-header\">\r\n<h5 class=\"modal-title\" id=\"offer_modal_title\">FOODPANDa</h5>\r\n</div>\r\n<div class=\"modal-body\">\r\n<div class=\"modal-detail\">\r\n<p><b>OFFER</b>:</p>\r\n<p>20% SAVINGS UP TO BDT 150 PER TRANSACTION WITH A MINIMUM ORDER OF BDT 499 FOR CITY BANK AMERICAN EXPRESS PLATINUM, GOLD AND BLUE CREDIT CARDS AND CITYMAXX CARD. APPLICABLE FOR TWO TRANSACTIONS ONLY.<br /><strong>FOR PANDAMART:</strong><span>&nbsp;</span>12% SAVINGS UP TO BDT 250/TRANSACTION WITH A MINIMUM TRANSACTION OF BDT 2000. APPLICABLE FOR TWO TRANSACTIONS ONLY.<br />FOR LAST 5 DAYS OF RAMADAN BDT 500 ON TRANSACTION OF ABOVE BDT 5,000. APPLICABLE FOR ONE TRANSACTION ONLY AND VALID FOR CITY BANK AMERICAN EXPRESS PLATINUM, GOLD &amp; BLUE CREDIT CARDS</p>\r\n</div>\r\n<p><b>WEB:<span>&nbsp;</span></b><a href=\"https://www.foodpanda.com.bd/\">https://www.foodpanda.com.bd/</a></p>\r\n<b>VALIDITY:</b><span>&nbsp;</span>TILL THE EVE OF EID-UL-FITR 2025\r\n<p><b>Eligible Cards</b><span>&nbsp;</span>: CITY BANK AMERICAN EXPRESS PLATINUM, GOLD, BLUE, CITYMAXX CARD AND CITY ALO CREDIT CARDS.</p>\r\n</div>', NULL, 'https://dev.ailservers.com/elegant.com.bd/garments-apparels', '2025/05/6819d63f5d2c7_FOODPANDA.jpg', 'dining-offers', '1', NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, NULL, '2025-05-06 02:16:11', '2025-05-06 04:20:11'),
+(12, 1, 1, '2,3,4', '20% SAVINGS', 'test-1', '<div class=\"modal-header\">\r\n<h5 class=\"modal-title\" id=\"offer_modal_title\">FOODPANDa</h5>\r\n</div>\r\n<div class=\"modal-body\">\r\n<div class=\"modal-detail\">\r\n<p><b>OFFER</b>:</p>\r\n<p>20% SAVINGS UP TO BDT 150 PER TRANSACTION WITH A MINIMUM ORDER OF BDT 499 FOR CITY BANK AMERICAN EXPRESS PLATINUM, GOLD AND BLUE CREDIT CARDS AND CITYMAXX CARD. APPLICABLE FOR TWO TRANSACTIONS ONLY.<br /><strong>FOR PANDAMART:</strong><span>&nbsp;</span>12% SAVINGS UP TO BDT 250/TRANSACTION WITH A MINIMUM TRANSACTION OF BDT 2000. APPLICABLE FOR TWO TRANSACTIONS ONLY.<br />FOR LAST 5 DAYS OF RAMADAN BDT 500 ON TRANSACTION OF ABOVE BDT 5,000. APPLICABLE FOR ONE TRANSACTION ONLY AND VALID FOR CITY BANK AMERICAN EXPRESS PLATINUM, GOLD &amp; BLUE CREDIT CARDS</p>\r\n</div>\r\n<p><b>WEB:<span>&nbsp;</span></b><a href=\"https://www.foodpanda.com.bd/\">https://www.foodpanda.com.bd/</a></p>\r\n<b>VALIDITY:</b><span>&nbsp;</span>TILL THE EVE OF EID-UL-FITR 2025\r\n<p><b>Eligible Cards</b><span>&nbsp;</span>: CITY BANK AMERICAN EXPRESS PLATINUM, GOLD, BLUE, CITYMAXX CARD AND CITY ALO CREDIT CARDS.</p>\r\n</div>', NULL, 'https://dev.ailservers.com/elegant.com.bd/garments-apparels', '2025/05/6819d63f5d2c7_FOODPANDA.jpg', 'dining-offers', '1', NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, NULL, '2025-05-06 02:16:11', '2025-05-07 00:40:32'),
 (19, NULL, 1, '3', 'trdt', 'trdt', NULL, NULL, NULL, NULL, 'dining-offers', '1', NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, NULL, '2025-05-06 03:39:22', '2025-05-06 03:56:02'),
 (20, NULL, 1, '3', 'gsdfgdfg', 'gsdfgdfg', NULL, NULL, NULL, NULL, 'dining-offers', '1', NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, NULL, '2025-05-06 03:56:22', '2025-05-06 03:56:22'),
 (21, NULL, 1, ',4', 'Shahin', 'shahin', NULL, NULL, NULL, '2025/05/6819d122debe0_Dining-black.png', 'dining-offers', '1', NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, NULL, '2025-05-06 04:12:25', '2025-05-06 04:12:50'),
-(22, NULL, 1, ',5', 'test', 'test-2', NULL, NULL, NULL, NULL, 'online-offers', '1', NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, NULL, '2025-05-06 04:39:15', '2025-05-06 04:39:46');
+(22, NULL, 1, ',5', 'test', 'test-2', NULL, NULL, NULL, NULL, 'online-offers', '1', NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, NULL, '2025-05-06 04:39:15', '2025-05-06 04:39:46'),
+(23, 2, 1, '', 'Current Existing Offers', 'current-existing-offers', NULL, NULL, NULL, NULL, 'posts', '1', NULL, NULL, NULL, NULL, NULL, NULL, '', 'meghna_slider_current,meghna_type_current', NULL, '2025-05-21 04:26:18', '2025-05-21 04:32:32'),
+(24, NULL, 1, '', 'Dummy Post', 'dummy-post', NULL, NULL, NULL, '2025/05/6819b4f1b5f5b_banner1.webp', 'current-offer-slider', '1', NULL, NULL, NULL, NULL, NULL, NULL, '', NULL, NULL, '2025-05-21 04:29:12', '2025-05-21 04:29:39');
 
 -- --------------------------------------------------------
 
@@ -347,11 +350,12 @@ CREATE TABLE `posttypes` (
 --
 
 INSERT INTO `posttypes` (`id`, `user_id`, `category_main_id`, `name`, `slug`, `pt_content`, `pt_content_css`, `pt_thumbnail_path`, `status`, `category_id`, `title`, `content`, `excerpt`, `thumbnail_path`, `option_1`, `option_2`, `option_3`, `option_4`, `more_option_1`, `more_option_2`, `gallery_img`, `trash`, `in_menu_swh`, `menu_icon`, `in_dashboard`, `paginate`, `template`, `created_at`, `updated_at`) VALUES
-(2, 1, 0, 'Footer', 'footer', NULL, NULL, NULL, '1', 'Categories', 'Title', 'Content', 'Excerpt', 'Thumbnail', 'Optional Field', 'Optional Field', 'Optional Field', 'Optional Field', 'Optional Field', 'Optional Field', 'Gallery', NULL, '1', NULL, '0', NULL, NULL, '2025-05-06 00:02:57', '2025-05-06 00:02:57'),
+(2, 1, 0, 'Footer', 'footer', NULL, NULL, NULL, '1', 'Categories', 'Title', 'Content', 'Excerpt', 'Thumbnail', 'Optional Field', 'Optional Field', 'Optional Field', 'Optional Field', 'Optional Field', 'Optional Field', 'Gallery', NULL, '0', NULL, '0', NULL, 0, '2025-05-06 00:02:57', '2025-05-06 22:21:06'),
 (4, 1, 0, 'Meghna Slider', 'meghna-slider', NULL, NULL, NULL, '1', 'Categories', 'Title', 'Content', 'Excerpt', 'Thumbnail', 'Optional Field', 'Optional Field', 'Optional Field', 'Optional Field', 'Optional Field', 'Optional Field', 'Gallery', NULL, '1', NULL, '0', NULL, 0, '2025-05-06 00:47:27', '2025-05-06 00:49:03'),
-(5, 1, 0, 'Posts', 'posts', NULL, NULL, NULL, '1', 'Categories', 'Title', 'Content', 'Excerpt', 'Thumbnail', 'Optional Field', 'Optional Field', 'Optional Field', 'Optional Field', 'Optional Field', 'Optional Field', 'Gallery', NULL, '1', NULL, '0', NULL, 1, '2025-05-06 00:49:52', '2025-05-06 00:52:05'),
-(6, 1, 1, 'Dining Offers', 'dining-offers', '<p><img src=\"http://localhost/meghna/public/uploads/2025/05/6819d122debe0_Dining-black.png\" width=\"100\" height=\"100\" alt=\"\" /></p>', NULL, '2025/05/6819b4f1b5f5b_banner1.webp', '1', 'Category', 'Title', 'Editor', 'URL', 'Image', '#', '#', '#', '#', '#', '#', '#', NULL, '1', 'Offers', '1', NULL, 1, '2025-05-06 01:15:30', '2025-05-06 04:38:08'),
-(9, 1, 1, 'Online Offers', 'online-offers', NULL, NULL, '2025/05/6819b4c0bddf6_banner3.webp', '1', 'category', 'Title', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', NULL, '1', NULL, '1', NULL, 1, '2025-05-06 04:38:58', '2025-05-06 04:39:39');
+(5, 1, 0, 'Posts', 'posts', NULL, NULL, NULL, '1', 'Categories', 'Title', 'Content', 'Excerpt', 'Thumbnail', 'Optional Field', 'Optional Field', 'Optional Field', 'Optional Field', 'Optional Field', 'Optional Field', 'Gallery', NULL, '0', NULL, '0', NULL, 1, '2025-05-06 00:49:52', '2025-05-07 00:36:39'),
+(6, 1, 1, 'Dining Offers', 'dining-offers', '<p><img src=\"http://localhost/meghna/public/uploads/2025/05/6819d122debe0_Dining-black.png\" width=\"100\" height=\"100\" alt=\"\" /></p>', NULL, '2025/05/6819b4f1b5f5b_banner1.webp', '1', 'Category', 'Title', 'Editor', 'URL', 'Image', '#', '#', '#', '#', '#', '#', '#', NULL, '0', NULL, '0', NULL, 1, '2025-05-06 01:15:30', '2025-05-21 04:20:39'),
+(9, 1, 6, 'Online Offers', 'online-offers', '<p><img src=\"http://localhost/meghna/public/uploads/2025/05/6819d122debe0_Dining-black.png\" width=\"100\" height=\"100\" alt=\"\" /></p>', NULL, '2025/05/6819b4c0bddf6_banner3.webp', '1', 'category', 'Title', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', NULL, '1', 'Dining Offers', '1', NULL, 1, '2025-05-06 04:38:58', '2025-05-21 04:46:44'),
+(11, 1, 0, 'Current Offer Slider', 'current-offer-slider', NULL, NULL, NULL, '1', 'Categories', 'Title', 'Content', 'Excerpt', 'Thumbnail', 'Optional Field', 'Optional Field', 'Optional Field', 'Optional Field', 'Optional Field', 'Optional Field', 'Gallery', NULL, '1', NULL, '0', NULL, 0, '2025-05-21 04:29:12', '2025-05-21 04:30:09');
 
 -- --------------------------------------------------------
 
@@ -397,7 +401,7 @@ CREATE TABLE `settings` (
 --
 
 INSERT INTO `settings` (`id`, `site_title`, `sub_title`, `site_logo`, `fav_icon`, `dashboard_color`, `text_color`, `text_hover`, `theme_url`, `home_url`, `editor`, `header`, `footer`, `created_at`, `updated_at`) VALUES
-(1, 'Meghna Bank PLC', NULL, '2025/05/6819a3885c98b_main-logo.png', '2025/05/6819a5679cea2_favicon.png', '#ffffff', '#37256e', '#837f90', 'meghna', '9', 'classic', 'meghna_header', 'meghna_footer', '2025-05-05 23:51:23', '2025-05-06 00:59:45');
+(1, 'Meghna Bank PLC', NULL, '2025/05/6819a3885c98b_main-logo.png', '2025/05/6819a5679cea2_favicon.png', '#ffffff', '#37256e', '#837f90', 'meghna', '9', 'classic', 'meghna_header', 'meghna_footer', '2025-05-05 23:51:23', '2025-05-07 00:39:29');
 
 -- --------------------------------------------------------
 
@@ -561,7 +565,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -609,13 +613,13 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `posttypes`
 --
 ALTER TABLE `posttypes`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `settings`
